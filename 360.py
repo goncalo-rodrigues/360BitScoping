1a148104e80643b92512a268320806ab0eee7216
psnint <pedro_migueld@hotmail.com>
2016-11-08 19:04:05 +0000

    Added code for multithreading purposes on 360.py

--- 360.py
+++ 360.py
@@ -4,8 +4,11 @@ import socket
 import sys, getopt
 from DPIComponent import DPIComponent
 from scapy.all import *
+import os
+import thread
 
-
+SIZE_THRESHOLD_MB = 1000
+SIZE_SPLIT_MB = 500
 
 def main(argv):
     output_file_path = ""
@@ -28,14 +31,41 @@ where possible options include:\n\
         print 'Missing argument: input capture file'
         sys.exit()
 
-    if output_file_path != "":
-        output_f = open(output_file_path, 'w+')
-        out_pcap = dpkt.pcap.Writer(output_f)
-    else:
+    in_pcap = args[0]
+    size_mb = os.stat(in_pcap).st_size/1024.0/1024.0
+
+#------------------------------------------------------------------------------
+#For multithreading purposes
+
+    if size_mb > SIZE_THRESHOLD_MB:
         out_pcap = None
         output_f = None
+        os.system("tcpdump -r "+in_pcap+" -w "+in_pcap+"_seg -C "+ str(SIZE_SPLIT_MB))
+        dir = os.path.dirname(in_pcap)
+        for file in os.listdir(dir):
+            ext = file[file.find(".pcap"):]
+            if "_seg" in ext:
+                seg = file[-1]
+                if seg == 'g':
+                    seg = 0
+                path = dir+"/"+file
+                output_f = open(output_file_path+"_seg"+str(seg), 'w+')
 
-    DPIComponent(args[0], out_pcap)
+                out_pcap = dpkt.pcap.Writer(output_f)
+                thread.start_new_thread(DPIComponent, (path, out_pcap))
+
+#------------------------------------------------------------------------------
+
+    else:
+        if output_file_path != "":
+            output_f = open(output_file_path, 'w+')
+            out_pcap = dpkt.pcap.Writer(output_f)
+        else:
+            out_pcap = None
+            output_f = None
+        DPIComponent(in_pcap, out_pcap)
+
+
 
     try:
         output_f.close()
